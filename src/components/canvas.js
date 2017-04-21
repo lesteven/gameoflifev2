214fbf103f7cb6c0be81ca10784e53f2e5f87dcd
steven <stevenle2011@gmail.com>
2017-04-21 19:28:55 +0700

    Add init and clear

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -13,6 +13,7 @@ class Canvas extends Component{
 		this.generate = this.generate.bind(this);
 		this.drawGrid = this.drawGrid.bind(this);
 		this.initGrid = this.initGrid.bind(this);
+		this.updateArray = this.updateArray.bind(this);
 	}
 	generate(){
 		let genArray = this.state.genArray;
@@ -21,12 +22,21 @@ class Canvas extends Component{
 		let genY = Math.floor(Math.random()*10)*cellSize;
 		let gen = [genX,genY];
 		genArray.push(gen);
-		console.log(genArray);
+		//console.log(genArray);
+	}
+	updateArray(index,num){
+		let grid = this.state.grid;
+		grid.splice(index,1)
+		grid.splice(index,0,num)
+		this.setState({grid:grid})
+		//console.log(this.state.grid);
 	}
 	colorCell(event){
 		const canvas = document.getElementById('canvas');
 		const ctx = canvas.getContext('2d');
 		const cellSize = this.state.cellSize;
+		const length = this.state.length;
+		const cellLength = length/cellSize;
 		//get coordinates of grid
 		const x = event.clientX - ctx.canvas.offsetLeft;
 		const y = event.clientY - ctx.canvas.offsetTop;
@@ -36,26 +46,36 @@ class Canvas extends Component{
 		//gets color of clicked cell
 		const cellColor = JSON.stringify(Array.from(ctx.getImageData(x,y,1,1).data));
 		const yellow = "[255,255,0,255]";
-		//fills cell with yellow if not yellow already
+		//finds index to replace in grid array
+		const replace = Math.floor(x/cellSize)+ Math.floor(y/cellSize)*cellLength;
 		if(cellColor !== yellow){
+			//fills in grid to prevent rerendering
 			ctx.fillStyle ='yellow';
-			ctx.fillRect(xrounded,yrounded,cellSize,cellSize);	
+			ctx.fillRect(xrounded,yrounded,cellSize,cellSize);
+			//console.log('replace:'+replace);
+			//update state of grid
+			this.updateArray(replace,1);
 		} else{
+			//fills in grid to prevent rerendering
 			ctx.fillStyle ='white';
 			ctx.fillRect(xrounded,yrounded,cellSize,cellSize);
+			//console.log('replace:'+replace);
+			//update state of grid
+			this.updateArray(replace,0);
 		}
-		console.log(Math.floor(x/cellSize),Math.floor(y/cellSize),
-			xrounded,yrounded,cellColor)	
+		/*console.log('x:'+Math.floor(x/cellSize),'y:'+Math.floor(y/cellSize),
+			xrounded,yrounded,cellColor)	*/
 	}
 	initGrid(){
 		const cellSize = this.state.cellSize;
 		const length = this.state.length;
 		const cellLength = length/cellSize;
-		const grid = this.state.grid;
+		const grid = [];
 		for(let i=0; i< cellLength*cellLength; i++){
 			grid.push(0);
 		}
-		this.setState({grid:grid})
+		this.setState({grid:grid});	
+		//console.log('init&&clear',this.state.grid);
 	}
 	drawGrid(){
 		const cellSize = this.state.cellSize;
@@ -67,19 +87,21 @@ class Canvas extends Component{
 		let ctx = canvas.getContext('2d');
 
 		grid.map((each,index)=>{
-			if(each===0){
-				console.log('each is 0',index);
+			if(each===0){		
 				ctx.rect(i,j,cellSize,cellSize);}
 			else if(each===1){
-				console.log('each is 1',index);
 				ctx.fillStyle ='yellow';
 				ctx.fillRect(i,j,cellSize,cellSize);}
 			if(i < length){i+=50;}
 			if(i===length){j+=cellSize;i=0}
 			return null;
 		})
-		
-		console.log(this.state.grid)
+		//console.log(this.state.grid)
+	}
+	clearGrid(){
+		let canvas = document.getElementById('canvas');
+		let ctx = canvas.getContext('2d');
+		ctx.clearRect(0,0,canvas.width,canvas.height);
 	}
 	componentDidMount(){
 		this.initGrid();
@@ -89,7 +111,13 @@ class Canvas extends Component{
 
 		return(
 			<div>
-
+			<div>
+				<button className="run">Run</button>
+				<button className="pause">Pause</button>
+				<button className="clear"
+				onClick={()=>{this.initGrid();this.clearGrid()}}
+				>Clear</button>
+			</div>
 				<canvas id="canvas"
 				onClick={this.colorCell}
 				width={this.state.length}
